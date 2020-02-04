bc578bc5331bee90041041a022845601499f133c
alex quesada <a.quemen@gmail.com>
2020-02-04 13:42:15 +0100

    init commit

---
+++ src/main/java/com/include/easydocker/EasyDockerApplication.java
@@ -0,0 +1,13 @@
+package com.include.easydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyDockerApplication.class, args);
+	}
+
+}
