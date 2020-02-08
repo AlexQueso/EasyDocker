8e1c7269b6a89dbe98763b63827279625360cc45
JoseZamora97 <josezamora94@yahoo.es>
2020-02-08 05:09:42 +0100

    Ended: Home page and logic for logging. Added: Headers for utils.

--- src/main/java/com/include/easydocker/controller/TemplateOverviewController.java
+++ src/main/java/com/include/easydocker/controller/TemplateOverviewController.java
@@ -11,24 +11,24 @@ public class TemplateOverviewController {
     @RequestMapping("/Template/{id}")
     public String templatePage(@PathVariable(value = "id")long templateId, Model model){
 
-        return "template_overview";
+        return "app";
     }
 
     @RequestMapping("/add_docker")
     public String addDocker(Model model){
 
-        return "template_overview";
+        return "app";
     }
 
     @RequestMapping("/edit_docker/{id}")
     public String editDocker(@PathVariable(value = "id")long dockerId, Model model){
 
-        return "template_overview";
+        return "app";
     }
 
     @RequestMapping("/delete_docker/{id}")
     public String deleteDocker(@PathVariable(value = "id")long dockerId, Model model){
 
-        return "template_overview";
+        return "app";
     }
 }
