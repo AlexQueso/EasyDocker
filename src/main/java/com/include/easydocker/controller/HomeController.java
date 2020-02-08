4931cb63ff1b866ab416b4975fb25a7c8684cf40
JoseZamora97 <josezamora94@yahoo.es>
2020-02-08 16:47:04 +0100

    Ended: Logic new users and HttpSession

--- src/main/java/com/include/easydocker/controller/HomeController.java
+++ src/main/java/com/include/easydocker/controller/HomeController.java
@@ -1,56 +1,81 @@
 package com.include.easydocker.controller;
 
+import com.include.easydocker.classes.User;
+import com.include.easydocker.services.UsersService;
 import com.include.easydocker.utils.Hasher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedList;
+
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @Autowired
+    private UsersService usersService;
+
+    @GetMapping("/")
     public String home(Model model){
         model.addAttribute("selection", true);
         return "home";
     }
 
-    @RequestMapping("/sign_in")
+    @GetMapping("/sign_in")
     public String changeToSignInView(Model model){
         model.addAttribute("sign-in", true);
         return "home";
     }
 
-    @RequestMapping("/sign_up")
+    @GetMapping("/sign_up")
     public String changeToSignUpView(Model model){
         model.addAttribute("sign-up", true);
         return "home";
     }
 
-    @RequestMapping("/signing_up")
-    public String signUp(Model model, @RequestParam String user, @RequestParam String password) {
+    @PostMapping(value = "/signing_up")
+    public String signUp(Model model, @RequestParam String user, @RequestParam String password ) {
 
-        if (!checkUser(user, Hasher.hash(password)))
-            return "app";
+        String hashedPwd = Hasher.hash(password);
+        User newUser = checkUser(user, hashedPwd);
+
+        if (newUser == null) {
+
+            newUser = new User(user, hashedPwd, new LinkedList<>());
+
+            // TODO: Registrar el usuario en la base de datos.
+
+            usersService.setUser(newUser);
+
+            return "redirect:/user-overview";
+        }
 
         model.addAttribute("sign-up", true);
 
         return "home";
     }
 
-    @RequestMapping("/signing_in")
-    public String signIn(Model model, @RequestParam String user, @RequestParam String password) {
+    @PostMapping(value = "/signing_in")
+    public String signIn(Model model, @RequestParam String user, @RequestParam String password ) {
 
-        if (checkUser(user, Hasher.hash(password)))
-            return "app";
+        User welcomeUser = checkUser(user, Hasher.hash(password));
+
+        if (welcomeUser != null) {
+            usersService.setUser(welcomeUser);
+            return "redirect:/user-overview";
+        }
 
         model.addAttribute("sign-in", true);
 
         return "home";
     }
 
-    private boolean checkUser(String user, String hash) {
-        // Todo: buscar en la base de datos y devover true si está y falso si no.
-        return false;
+    private User checkUser(String user, String hash) {
+        // Todo: buscar en la base de datos y devover el user, o null si no se encuentra.
+        return null;
     }
 }
