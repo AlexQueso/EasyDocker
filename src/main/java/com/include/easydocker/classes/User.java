b11edf9be665d339519e0d22ba0390daa933fd42
Alex Quesada <a.quemen@gmail.com>
2020-02-12 21:00:02 +0100

    Fix: recent-signed users problem to create projects Added: createVolume, createNetwork and createService methods in AppController

--- src/main/java/com/include/easydocker/classes/User.java
+++ src/main/java/com/include/easydocker/classes/User.java
@@ -1,7 +1,6 @@
 package com.include.easydocker.classes;
 
 import javax.persistence.*;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
--- src/main/java/com/include/easydocker/controller/AppController.java
+++ src/main/java/com/include/easydocker/controller/AppController.java
@@ -1,8 +1,6 @@
 package com.include.easydocker.controller;
 
-import com.include.easydocker.classes.Project;
-import com.include.easydocker.classes.Template;
-import com.include.easydocker.classes.User;
+import com.include.easydocker.classes.*;
 import com.include.easydocker.repositories.ProjectRepository;
 import com.include.easydocker.repositories.TemplateRepository;
 import com.include.easydocker.repositories.UserRepository;
@@ -27,36 +25,18 @@ public class AppController {
 
     @GetMapping("/user-overview")
     public String userPage(Model model) {
-        /*
-        appService.showLoggedInfoOrTemporal(model);
-
-        model.addAttribute("user", true);
-        model.addAttribute("projects", usersSession.getUser().getProjects());
-        */
         appService.userPage(model);
         return "app";
-
     }
 
     @PostMapping(value = "/new-project")
     public String createProject(Project project) {
-        /*
-        project.setUser(usersSession.getUser());
-        if (!usersSession.getUser().getName().equals(User.UNKNOWN)){
-            projectRepository.save(project);
-        } else {
-            usersSession.getUser().getProjects().add(project);
-        }*/
         appService.createProject(project);
         return "redirect:/user-overview";
     }
 
     @PostMapping(value = "/new-template/{idProject}")
     public String createTemplate(@PathVariable long idProject, Template template) {
-        /*
-        template.setProject(projectRepository.findById(idProject));
-        templateRepository.save(template);
-        */
         appService.createTemplate(idProject, template);
         return "redirect:/project/" + idProject;
     }
@@ -69,45 +49,18 @@ public class AppController {
 
     @GetMapping("/project/{id}")
     public String projectOverview(@PathVariable long id, Model model) {
-        /*
-        showLoggedInfoOrTemporal(model);
-
-        Project p = projectRepository.findById(id);
-
-        model.addAttribute("templates", p.getTemplates());
-        model.addAttribute("idProject", p.getId());
-
-        model.addAttribute("project", true);
-         */
         appService.projectOverview(id, model);
         return "app";
     }
 
     @GetMapping("/template/{id}")
     public String templateOverview(@PathVariable long id, Model model) {
-        /*
-        showLoggedInfoOrTemporal(model);
-
-        Template t = templateRepository.findById(id);
-
-        model.addAttribute("idTemplate", t.getId());
-        model.addAttribute("template", true);
-        */
         appService.templateOverview(id, model);
         return "app";
     }
 
     @GetMapping("/user-temporal")
     public String temporalPage(Model model, HttpSession session) {
-        /*
-        User unknownUser = new User();
-        unknownUser.setId(System.currentTimeMillis());
-        unknownUser.setName(User.UNKNOWN);
-        unknownUser.setProjects(new LinkedList<>());
-
-        usersSession.setUser(unknownUser);
-        session.setAttribute("user", unknownUser);
-        */
         appService.temporalPage(model);
         return "redirect:/user-overview";
     }
