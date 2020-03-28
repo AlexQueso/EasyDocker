65e150b783bc14ca59a646d3ca88e406a924028e
JoseZamora97 <josezamora94@yahoo.es>
2020-03-29 00:45:17 +0100

    Added: Spring security TLS

---
+++ src/main/java/com/include/easydocker/security/CSRFHandlerInterceptor.java
@@ -0,0 +1,17 @@
+package com.include.easydocker.security;
+
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CSRFHandlerInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+        modelAndView.addObject("token", token.getToken());
+    }
+}
