4922e2dbc9fe3ca5caf4ad1d040b527dd8f82e55
JoseZamora97 <josezamora94@yahoo.es>
2020-03-31 01:57:36 +0200

    delete: deployment folder

--- src/main/java/com/include/easydocker/utils/WebSocketHandler.java
+++ src/main/java/com/include/easydocker/utils/WebSocketHandler.java
@@ -1,18 +1,11 @@
 package com.include.easydocker.utils;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.include.easydocker.session.UsersSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-
-import static java.lang.Thread.sleep;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
